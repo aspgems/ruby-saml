352594e3b23234d29968ecd76813ad50310a446f
arctarus <arctarusnet@gmail.com>
2013-06-03 16:29:01 +0200

    move authrequest configuration to instance variable

--- lib/onelogin/ruby-saml/authrequest.rb
+++ lib/onelogin/ruby-saml/authrequest.rb
@@ -10,10 +10,14 @@ module Onelogin
   include REXML
     class Authrequest
 
-      attr_reader :base64_request, :request_doc
+      attr_reader :base64_request
 
-      def create(settings, params = {})
-        @request_doc = create_authentication_xml_doc(settings)
+      def initialize(settings)
+        @settings = settings
+      end
+
+      def create(params = {})
+        request_doc = xml_doc
 
         request = ""
         request_doc.write(request)
@@ -23,19 +27,19 @@ module Onelogin
         deflated_request  = Zlib::Deflate.deflate(request, 9)[2..-5]
         @base64_request    = Base64.encode64(deflated_request)
         encoded_request   = CGI.escape(base64_request)
-        params_prefix     = (settings.idp_sso_target_url =~ /\?/) ? '&' : '?'
+        params_prefix     = (@settings.idp_sso_target_url =~ /\?/) ? '&' : '?'
         request_params    = "#{params_prefix}SAMLRequest=#{encoded_request}"
 
         params.each_pair do |key, value|
           request_params << "&#{key}=#{CGI.escape(value.to_s)}"
         end
 
-        settings.idp_sso_target_url + request_params
+        @settings.idp_sso_target_url + request_params
       end
 
-      def create_authentication_xml_doc(settings)
+      def xml_doc
         uuid = "_" + UUID.new.generate
-        time = Time.now.utc.strftime("%Y-%m-%dT%H:%M:%S")
+        time = Time.now.strftime("%Y-%m-%dT%H:%M:%SZ")
         # Create AuthnRequest root element using REXML 
         request_doc = REXML::Document.new
         request_doc.context[:attribute_quote] = :quote
@@ -47,32 +51,32 @@ module Onelogin
         root.attributes['ID'] = uuid
         root.attributes['IssueInstant'] = time
         root.attributes['Version'] = "2.0"
-        root.attributes['Destination'] = settings.idp_sso_target_url unless settings.idp_sso_target_url.nil?
-        root.attributes['IsPassive'] = settings.passive unless settings.passive.nil?
-        root.attributes['ProviderName'] = settings.provider_name unless settings.provider_name.nil?
-        root.attributes['ProtocolBinding'] = settings.protocol_binding unless settings.protocol_binding.nil?
+        root.attributes['Destination'] = @settings.idp_sso_target_url unless @settings.idp_sso_target_url.nil?
+        root.attributes['IsPassive'] = @settings.passive unless @settings.passive.nil?
+        root.attributes['ProviderName'] = @settings.provider_name unless @settings.provider_name.nil?
+        root.attributes['ProtocolBinding'] = @settings.protocol_binding unless @settings.protocol_binding.nil?
 
         # Conditionally defined elements based on settings
-        if settings.assertion_consumer_service_url != nil
-          root.attributes["AssertionConsumerServiceURL"] = settings.assertion_consumer_service_url
+        if @settings.assertion_consumer_service_url != nil
+          root.attributes["AssertionConsumerServiceURL"] = @settings.assertion_consumer_service_url
         end
-        if settings.issuer != nil
+        if @settings.issuer != nil
           issuer = root.add_element "saml:Issuer", { "xmlns:saml" => "urn:oasis:names:tc:SAML:2.0:assertion" }
-          issuer.text = settings.issuer
+          issuer.text = @settings.issuer
         end
-        if settings.name_identifier_format != nil
+        if @settings.name_identifier_format != nil
           root.add_element "samlp:NameIDPolicy", { 
               "xmlns:samlp" => "urn:oasis:names:tc:SAML:2.0:protocol",
               # Might want to make AllowCreate a setting?
               "AllowCreate" => "true",
-              "Format" => settings.name_identifier_format
+              "Format" => @settings.name_identifier_format
           }
         end
 
         # BUG fix here -- if an authn_context is defined, add the tags with an "exact"
         # match required for authentication to succeed.  If this is not defined, 
         # the IdP will choose default rules for authentication.  (Shibboleth IdP)
-        if settings.authn_context != nil
+        if @settings.authn_context != nil
           requested_context = root.add_element "samlp:RequestedAuthnContext", { 
             "xmlns:samlp" => "urn:oasis:names:tc:SAML:2.0:protocol",
             "Comparison" => "exact",
@@ -80,9 +84,13 @@ module Onelogin
           class_ref = requested_context.add_element "saml:AuthnContextClassRef", { 
             "xmlns:saml" => "urn:oasis:names:tc:SAML:2.0:assertion",
           }
-          class_ref.text = settings.authn_context
+          class_ref.text = @settings.authn_context
         end
         request_doc
+      end
+
+      def encoded_xml
+        Base64.encode64(xml_doc.to_s).gsub("\n","")
       end
 
     end
